88132d7066d2e2e83f2c47327badf484b53bed25
gujun <gujun@qiyi.com>
2020-06-16 10:39:53 +0800

    add other StringUtils.

---
+++ src/main/java/com/jsgygujun/code/string_utilities/ExpCharMatcher.java
@@ -0,0 +1,8 @@
+package com.jsgygujun.code.string_utilities;
+
+/**
+ * @author gujun@qiyi.com
+ * @since 2020/6/16 10:37 上午
+ */
+public class ExpCharMatcher {
+}
---
+++ src/main/java/com/jsgygujun/code/string_utilities/ExpStrings.java
@@ -0,0 +1,8 @@
+package com.jsgygujun.code.string_utilities;
+
+/**
+ * @author gujun@qiyi.com
+ * @since 2020/6/16 10:38 上午
+ */
+public class ExpStrings {
+}
