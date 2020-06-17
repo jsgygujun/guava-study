6bdb69a2e886d235d54d8411c3e8c9be0349526f
gujun <gujun@qiyi.com>
2020-06-17 16:04:51 +0800

    add AsyncEventBus demo.

--- src/main/java/com/jsgygujun/code/event_bus/ExpAsyncEventBus.java
+++ src/main/java/com/jsgygujun/code/event_bus/ExpAsyncEventBus.java
@@ -1,13 +1,17 @@
 package com.jsgygujun.code.event_bus;
 
+import com.google.common.eventbus.AllowConcurrentEvents;
+import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 
+import java.util.concurrent.Executors;
+
 /**
  * @author gujun@qiyi.com
- * @since 2020/6/16 11:26 上午
+ * @since 2020/6/17 4:03 下午
  */
-public class ExpEventBus {
+public class ExpAsyncEventBus {
     // 自定义事件
     public static class Event {
         private String message;
@@ -27,18 +31,19 @@ public class ExpEventBus {
         }
         // 订阅方法，必须使用注解声明，且只能有一个参数，当有消息发布的时候，该方法自动被调用。
         @Subscribe
-        public void onMessage(Event event) {
+        @AllowConcurrentEvents
+        public void onMessage(ExpEventBus.Event event) {
             System.out.println("Thread:" + Thread.currentThread().getName() + ", name: " + name  + " received a message: " + event.getMessage());
         }
     }
 
     public static void main(String[] args) {
-        // 定义EventBus对象
-        final EventBus eventBus = new EventBus("EventBus");
+        // 定义异步EventBus对象
+        final AsyncEventBus asyncEventBus = new AsyncEventBus(Executors.newSingleThreadExecutor());
         // 向EventBus对象注册监听对象
-        eventBus.register(new EventListener("listener1"));
-        eventBus.register(new EventListener("listener2"));
+        asyncEventBus.register(new ExpEventBus.EventListener("listener1"));
+        asyncEventBus.register(new ExpEventBus.EventListener("listener2"));
         // 发布一个事件，该事件会通知到所有注册的监听者
-        eventBus.post(new Event("Hello every listener, this is a test message!"));
+        asyncEventBus.post(new ExpEventBus.Event("Hello every listener, this is a test message!"));
     }
 }
